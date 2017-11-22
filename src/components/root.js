04dfb1472c0b2477e03d828c76c2141f8188384e
vzvu3k6k <vzvu3k6k@gmail.com>
2017-11-23 02:42:54 +0900

    Simplify store composing

--- src/components/root.js
+++ src/components/root.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import reducers from "../reducers";
 import TodoList from "./todo_list";
 
-const store = compose(applyMiddleware(thunk))(createStore)(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default class Root extends Component {
   render() {

3b68cd264086dd3c222ad12d05ec6417e06b71bd
vzvu3k6k <vzvu3k6k@gmail.com>
2017-11-24 02:10:51 +0900

    Remove commented out code

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -45,5 +45,4 @@ function mapDispatchToProps(dispatch) {
   );
 }
 
-//export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
